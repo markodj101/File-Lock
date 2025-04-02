711b3ada118cc9739e375fa1544cafa37a0576be
markodj101 <markodj10@gmail.com>
2025-04-02 23:26:10 +0200

    Added messages for unlocking the file!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter
 import tkinter.filedialog
 import customtkinter
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet,InvalidToken, InvalidSignature
 import base64
-
+import binascii
 
 WRAP_PIXEL_WIDTH = 400
 
@@ -54,8 +54,8 @@ def handle_file_lock_up(root,file,button_frame,lock_frame):
     key = Fernet.generate_key()
     url_safe_key = base64.urlsafe_b64encode(key)
     
-    print(f"Fernet Key: {url_safe_key.decode()}")
-    key_output= customtkinter.CTkLabel(master=lock_frame, text=f"Your key is : \n{url_safe_key.decode()}",font=customtkinter.CTkFont(size=15,weight="bold"),wraplength=WRAP_PIXEL_WIDTH,  
+    
+    key_output= customtkinter.CTkLabel(master=lock_frame, text=f"Your key is : \n{url_safe_key}",font=customtkinter.CTkFont(size=15,weight="bold"),wraplength=WRAP_PIXEL_WIDTH,  
     justify="center")
     copy_button = customtkinter.CTkButton(master=lock_frame, text="Copy to Clipboard", command=lambda:copy_to_clipboard(root, url_safe_key.decode(), lock_frame))
     key_output.pack(pady=10,padx=10)
@@ -77,26 +77,65 @@ def handle_file_lock_up(root,file,button_frame,lock_frame):
 
 
 def handle_file_unlock(root,file,button_frame,unlock_frame):
-    
     back_button = customtkinter.CTkButton(master=root, text="Go Back!", command=lambda:home_page(root,button_frame,unlock_frame,back_button))
     back_button.pack(pady=10,padx=10)
 
     input = customtkinter.CTkEntry(master=unlock_frame,placeholder_text="Type in the key:")
     input.pack(pady=10,padx=10)
-
     
-    key = "Tnl6dkVUeWVqYU56SWdOR0NIcTNITlk1X0xBV0hhVDBJaFNGcXdSRHFCMD0="
-    key = base64.urlsafe_b64decode(key)
-    fernet = Fernet(key)
-
-
-    with open(file, 'rb') as fl:
-        encrypted = fl.read()
     
-    decrypted = fernet.decrypt(encrypted)
+    validate= customtkinter.CTkButton(master=unlock_frame,text="Unlock file!",fg_color="green", command=lambda:unlock_file(root,unlock_frame,input.get(),file))
+    validate.pack(pady=10,padx=10)
+    
+    
 
-    with open(file, 'wb') as dec:
-        dec.write(decrypted)
+
+def unlock_file(root,unlock_frame, input,file):
+    for widget in unlock_frame.winfo_children():
+        if isinstance(widget, customtkinter.CTkLabel):
+            widget.destroy()
+    print("Trying to unlock the file.")
+    print(input)
+    response = customtkinter.CTkLabel(master=unlock_frame)
+    response.pack(pady=10,padx=10)
+    if not input:
+        response.configure(text="Field is empty!", text_color="red")
+       
+    else:
+        try:
+            key = base64.urlsafe_b64decode(input)
+            if len(key) <= 32:
+                print(f"Key lenghty {len(key)}")
+                response.configure(text="Invalid key lenght", text_color="red")
+                
+            else:
+                fernet = Fernet(key)
+    
+                with open(file, 'rb') as fl:
+                    encrypted = fl.read()
+
+                try:
+                    
+                    decrypted = fernet.decrypt(encrypted)
+
+                    with open(file, 'wb') as dec:
+                        dec.write(decrypted)
+
+                    response.configure(text="File decrypted successfully!", text_color="green")
+                    
+                
+                except(InvalidToken, InvalidSignature):
+                    response.configure(text="Invalid key - decryption failed", text_color="red")
+                    
+        except(ValueError, binascii.Error):
+            response.configure(text="Invalid key format (not base64)", text_color="red")
+            response.pack(pady=10,padx=10)
+
+        except Exception as e:
+            response.configure(text=f"Error: {str(e)}", text_color="red")
+
+
+
 
 def copy_to_clipboard(root, key, lock_frame):
     
